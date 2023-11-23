0a43d3ec04d951a6a9583ef22d5addcff656cf46
bboyleonp666 <qw80818181@gmail.com>
2023-11-23 22:10:43 +0800

    Make spot and futures script both available
    1. modify the import location for futures and spot
       scripts

---
+++ testnet_futures.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import os
+
+from binance_api.binance_functions import BINANCE_CLIENT, BINANCE_REQUEST
+from binance_api.utils import load_private_key, load_secret_key
+from dotenv import load_dotenv
+
+# Set up the default variables
+load_dotenv()
+FUTURES_API_KEY = os.getenv('FUTURES_API_KEY')
+FUTURES_SECRET_KEY = os.getenv('FUTURES_SECRET_KEY')
+
+
+def main():
+    secret_key = load_secret_key(FUTURES_SECRET_KEY)
+    client = BINANCE_CLIENT(api_key=FUTURES_API_KEY, secret_key=secret_key, futures=True, testnet=True)
+
+    # Set up the request parameters
+    data = {
+        'symbol':      'BTCUSDT',
+        # 'interval':     '1m',
+        # 'side':         'BUY',
+        # 'type':         'LIMIT',
+        # 'timeInForce':  'GTC',
+        # 'quantity':     '1.0000000',
+        # 'price':        '8000',
+        # 'recvWindow':   '5000',
+    }
+
+    # res = client.send(BINANCE_REQUEST.get_price, data=data)
+    # res = client.send(BINANCE_REQUEST.get_open_orders, data=data)
+    # res = client.send(BINANCE_REQUEST.get_account)    # /fapi/v1/account is no longer supported, use /fapi/v2/account instead
+    # res = client.send(BINANCE_REQUEST.get_open_orders, data=data)
+    # res = client.send(BINANCE_REQUEST.place_order, data=data)
+    # res = client.send(BINANCE_REQUEST.get_exchange_info)
+    res = client.send(BINANCE_REQUEST.get_historical_trades, data=data)
+    # res = client.send(BINANCE_REQUEST.get_all_orders, data=data)
+    # res = client.send(BINANCE_REQUEST.get_klines, data=data)
+
+    # data = {
+    #     'symbol': 'BTCUSDT',
+    #     'orderId': 1222,
+    # }
+    # res = client.send(BINANCE_REQUEST.get_order, data=data)
+    # res = client.send(BINANCE_REQUEST.delete_order, data=data)
+    # res = client.send(BINANCE_REQUEST.get_average_price, data=data) # not supported by futures
+    # res = client.send(BINANCE_REQUEST.get_agg_trades, data=data)
+    # res = client.send(BINANCE_REQUEST.get_my_allocations, data=data)
+    # res = client.send(BINANCE_REQUEST.get_all_order_list) # not supported by futures
+
+    print('Status Code:', res.status_code)
+    print(res.json())
+
+
+if __name__ == '__main__':
+    main()
---
+++ testnet_spot.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+import os
+# import time
+# import base64
+# import requests
+
+# from binance_api.binance_variables import *
+from binance_api.binance_functions import BINANCE_CLIENT, BINANCE_REQUEST
+from binance_api.utils import load_private_key, load_secret_key
+from dotenv import load_dotenv
+
+# Set up the default variables
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+PRIVATE_KEY_PATH = os.getenv('PRIVATE_KEY_PATH')
+
+
+def main():
+    private_key = load_private_key(PRIVATE_KEY_PATH)
+    client = BINANCE_CLIENT(api_key=API_KEY, private_key=private_key, futures=False, testnet=True)
+
+    # Set up the request parameters
+    data = {
+        'symbol':       'BTCUSDT',
+        # 'interval':     '1m',
+        'side':         'BUY',
+        'type':         'LIMIT',
+        'timeInForce':  'GTC',
+        'quantity':     '1.0000000',
+        'price':        '8000',
+        'recvWindow':   '5000',
+    }
+
+    res = client.send(BINANCE_REQUEST.get_price, data=data)
+    # res = client.send(BINANCE_REQUEST.get_open_orders, data=data)
+    # res = client.send(BINANCE_REQUEST.get_account)
+    # res = client.send(BINANCE_REQUEST.get_open_orders, data=data)
+    # res = client.send(BINANCE_REQUEST.place_order, data=data)
+    # res = client.send(BINANCE_REQUEST.get_exchange_info)
+    # res = client.send(BINANCE_REQUEST.get_historical_trades, data=data)
+    # res = client.send(BINANCE_REQUEST.get_all_orders, data=data)
+    # res = client.send(BINANCE_REQUEST.get_klines, data=data)
+
+    # data = {
+    #     'symbol': 'BTCUSDT',
+    #     'orderId': 1222,
+    # }
+    # res = client.send(BINANCE_REQUEST.get_order, data=data)
+    # res = client.send(BINANCE_REQUEST.delete_order, data=data)
+    # res = client.send(BINANCE_REQUEST.get_average_price, data=data)
+    # res = client.send(BINANCE_REQUEST.get_agg_trades, data=data)
+    # res = client.send(BINANCE_REQUEST.get_my_allocations, data=data)
+    # res = client.send(BINANCE_REQUEST.get_all_order_list)
+
+    print('Status Code:', res.status_code)
+    print(res.json())
+
+
+    # res = client.send(BINANCE_REQUEST.get_average_price, data=data)
+    # print('Status Code:', res.status_code)
+    # print(res.json())
+
+    
+    # res = client.send(BINANCE_REQUEST.get_exchange_info)
+    # for symbol_info in res.json()['symbols']:
+    #     if symbol_info['symbol'] == 'BTCUSDT':
+    #         for filter in symbol_info['filters']:
+    #             if filter['filterType'] == 'PERCENT_PRICE_BY_SIDE':
+    #                 print(filter)
+    #                 break
+    #             # print(filter)
+    
+
+if __name__ == '__main__':
+    main()
