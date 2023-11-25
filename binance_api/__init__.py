3676486eea88e5c11872cabcf92d94a28f70b85d
bboyleonp666 <qw80818181@gmail.com>
2023-11-25 16:31:44 +0800

    Add often used objects to __init__

--- binance_api/__init__.py
+++ binance_api/__init__.py
@@ -1,1 +1,3 @@
 from .history import History
+from .functions import BINANCE_CLIENT, BINANCE_REQUEST
+from .utils import load_secret_key, load_private_key
--- testnet_futures.py
+++ testnet_futures.py
@@ -2,7 +2,7 @@
 
 import os
 
-from binance_api.functions import BINANCE_CLIENT, BINANCE_REQUEST
+from binance_api import BINANCE_CLIENT, BINANCE_REQUEST
 from binance_api.utils import load_private_key, load_secret_key
 from dotenv import load_dotenv
 
